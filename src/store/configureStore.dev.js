266efb2e864719cb5454e183fb9989e128110ed5
Rene Rosa <realpasro09@hotmail.com>
2018-10-18 18:42:42 -0600

    initial commit

--- src/store/configureStore.dev.js
+++ src/store/configureStore.dev.js
@@ -4,6 +4,8 @@ import promise from 'redux-promise'
 import { createStore, applyMiddleware } from 'redux'
 import reducers from '../reducers'
 
+// Tip: check this post: https://www.javascriptjanuary.com/blog/may-cause-side-effects-how-to-implement-redux-sagas-as-middleware
+
 // Tip: replace the thunk and promise middleware with the redux-saga middleware
 
 const configureStore = () => {
